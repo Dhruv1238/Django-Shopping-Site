04d02eb21df2452a92fa1e6d70d42233f11d1560
Dhruv Sharma <dhruv4075@gmail.com>
2023-01-18 15:40:03 +0530

    some models

--- basic/settings.py
+++ basic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'segragation',
-    'shop'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#managing media
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL='media/' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ 
